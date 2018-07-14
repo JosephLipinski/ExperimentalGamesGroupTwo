71507dccfdd246afc1083101352d9bd4ca2b3b48
Joseph S. Lipinski <jsl94@drexel.edu>
2018-07-14 18:59:27 -0400

    Sample Scene
    Sample scene with some basic movement
    
    Left click to navigate to a location
    Q/E to swap the active character

---
+++ Trench Coat/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+
+    public Camera _camera;
+    NavMeshAgent agent;
+
+    public enum State {
+        Idle,
+        Move
+    }
+
+    State _state;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(FSM());
+        agent = GetComponent<NavMeshAgent>();
+	}
+
+    IEnumerator FSM(){
+        while(true){
+            switch(_state){
+                case State.Idle:
+                    Idle();
+                    break;
+                case State.Move:
+                    Move();
+                    break;
+            }
+            yield return null;
+        }
+    }
+
+    void Idle(){
+        
+    }
+
+    void Move(){
+        if(Input.GetMouseButtonDown(0)){
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit)){
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+
+    public void SetState(State _passedState){
+        _state = _passedState;
+    }
+}
---
+++ Trench Coat/Assets/Scripts/SwitchPlayer.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchPlayer : MonoBehaviour {
+    
+    GameObject PlayerGroup;
+    GameObject R, B, G;
+    public PlayerController rScript, gScript, bScript;
+    public int character = 0;
+    public Camera _mainCamera;
+
+    private void Start()
+    {
+        PlayerGroup = this.gameObject;
+
+        R = PlayerGroup.transform.GetChild(0).gameObject;
+        rScript = R.GetComponent<PlayerController>();
+
+        G = PlayerGroup.transform.GetChild(1).gameObject;
+        gScript = G.GetComponent<PlayerController>();
+
+        B = PlayerGroup.transform.GetChild(2).gameObject;GetComponent<PlayerController>();
+        bScript = B.GetComponent<PlayerController>();
+
+        SetActive();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if(Input.GetKeyDown(KeyCode.Q)){
+            if(character > 0){
+                character--;
+                SetActive();
+            } else {
+                character = 2;
+                SetActive();
+            }
+
+        }
+        else if(Input.GetKeyDown(KeyCode.E)){
+            if (character < 2){
+                character++;
+                SetActive();
+            } else {
+                character = 0;
+                SetActive();
+            }
+        }
+	}
+
+    void SetActive(){
+        switch(character){
+            case 0:
+                rScript.SetState(PlayerController.State.Move);
+                bScript.SetState(PlayerController.State.Idle);
+                gScript.SetState(PlayerController.State.Idle);
+                break;
+            case 1:
+                rScript.SetState(PlayerController.State.Idle);
+                bScript.SetState(PlayerController.State.Move);
+                gScript.SetState(PlayerController.State.Idle);
+                break;
+            case 2:
+                rScript.SetState(PlayerController.State.Idle);
+                bScript.SetState(PlayerController.State.Idle);
+                gScript.SetState(PlayerController.State.Move);
+                break;
+        }
+
+    }
+
+
+}
